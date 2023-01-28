aaa11db94b68406bdbeabfa19775e1520a0f57bc
ravi-0841 <ravishankar0841@gmail.com>
2023-01-28 14:13:58 -0500

    masking on encoder output directly

--- encoder_segmentation_decoder.py
+++ encoder_segmentation_decoder.py
@@ -152,6 +152,41 @@ class GluConvTranspose2d(nn.Module):
 
 
 class EncoderDecoder(nn.Module):
+    # def __init__(self):
+    #     super(EncoderDecoder, self).__init__()
+
+    #     self.conv1_enc = GluConv1d(in_channels=257, out_channels=128, 
+    #                                kernel_size=7, stride=1)
+    #     self.conv2_enc = GluConv1d(in_channels=128, out_channels=256, 
+    #                                kernel_size=5, stride=1)
+    #     self.encoder_mha = nn.MultiheadAttention(embed_dim=256, num_heads=4, 
+    #                                              dropout=0.5)
+        
+    #     self.encoder_linear = nn.Linear(in_features=256, out_features=1)
+        
+    #     self.sigmoid_activation = nn.Sigmoid()
+        
+    #     self.conv1_dec = GluConv1d(in_channels=257, out_channels=128, 
+    #                                kernel_size=5, stride=1)
+    #     self.conv2_dec = GluConv1d(in_channels=128, out_channels=256, 
+    #                                kernel_size=7, stride=1)
+    #     self.decoder_mha = nn.MultiheadAttention(embed_dim=256, num_heads=4,
+    #                                              dropout=0.5)
+        
+    #     self.decoder_linear = nn.Linear(in_features=256, out_features=257)
+
+    #     self.bn1_enc = nn.BatchNorm1d(128)
+    #     self.bn2_enc = nn.BatchNorm1d(256)
+    #     self.bn3_enc = nn.BatchNorm1d(256)
+
+    #     self.bn1_dec = nn.BatchNorm1d(128)
+    #     self.bn2_dec = nn.BatchNorm1d(256)
+    #     self.bn3_dec = nn.BatchNorm1d(256)
+
+    #     self.elu = nn.ELU(inplace=True)
+    #     # self.relu = nn.ReLU(inplace=True)
+
+
     def __init__(self):
         super(EncoderDecoder, self).__init__()
 
@@ -166,7 +201,7 @@ class EncoderDecoder(nn.Module):
         
         self.sigmoid_activation = nn.Sigmoid()
         
-        self.conv1_dec = GluConv1d(in_channels=257, out_channels=128, 
+        self.conv1_dec = GluConv1d(in_channels=256, out_channels=128, 
                                    kernel_size=5, stride=1)
         self.conv2_dec = GluConv1d(in_channels=128, out_channels=256, 
                                    kernel_size=7, stride=1)
@@ -187,38 +222,80 @@ class EncoderDecoder(nn.Module):
         # self.relu = nn.ReLU(inplace=True)
 
 
+    # def forward(self, x):
+
+    #     # x -> shape [batch, #stft bins, #time frames]
+    #     e1_enc = self.elu(self.bn1_enc(self.conv1_enc(x)))
+    #     e2_enc = self.elu(self.bn2_enc(self.conv2_enc(e1_enc)))
+    #     print("1. e2_enc shape: ", e2_enc.shape)
+
+    #     e2_enc = e2_enc.permute(2,0,1)
+    #     e3_enc, _ = self.encoder_mha(e2_enc, e2_enc, e2_enc)
+    #     print("2. e3_enc shape: ", e3_enc.shape)
+
+    #     e3_enc = e3_enc.permute(1,2,0)
+    #     e3_enc = self.bn3_enc(e3_enc)
+    #     posterior = self.encoder_linear(e3_enc.permute(0,2,1))
+    #     posterior = self.sigmoid_activation(posterior/10.)
+    #     sampler = torch.distributions.continuous_bernoulli.ContinuousBernoulli(probs=posterior)
+    #     print("3. posterior shape: ", posterior.shape)
+
+    #     sampled_val = sampler.sample()
+    #     mask = sampled_val.repeat(1, 1, 257)
+    #     print("4. mask shape: ", mask.shape)
+
+    #     enc_out = x * mask.permute(0,2,1)
+    #     e1_dec = self.elu(self.bn1_dec(self.conv1_dec(enc_out)))
+    #     e2_dec = self.elu(self.bn2_dec(self.conv2_dec(e1_dec)))
+    #     e2_dec = e2_dec.permute(2,0,1)
+    #     print("5. e2_dec shape: ", e2_dec.shape)
+
+    #     e3_dec, _ = self.decoder_mha(e2_dec, e2_dec, e2_dec)
+    #     e3_dec = e3_dec.permute(1,2,0)
+    #     print("6. e3_dec shape: ", e3_dec.shape)
+
+    #     e3_dec = self.bn3_dec(e3_dec)
+    #     out = self.decoder_linear(e3_dec.permute(0,2,1))
+    #     print("7. out shape: ", out.shape)
+
+    #     return posterior, sampled_val, out.permute(0,2,1)
+
+    
     def forward(self, x):
 
-        # out = x
+        # x -> shape [batch, #stft bins, #time frames]
         e1_enc = self.elu(self.bn1_enc(self.conv1_enc(x)))
         e2_enc = self.elu(self.bn2_enc(self.conv2_enc(e1_enc)))
-        # print("1. e2_enc shape: ", e2_enc.shape)
+        print("1. e2_enc shape: ", e2_enc.shape)
+
         e2_enc = e2_enc.permute(2,0,1)
         e3_enc, _ = self.encoder_mha(e2_enc, e2_enc, e2_enc)
-        # print("2. e3_enc shape: ", e3_enc.shape)
+        print("2. e3_enc shape: ", e3_enc.shape)
+
         e3_enc = e3_enc.permute(1,2,0)
         e3_enc = self.bn3_enc(e3_enc)
         posterior = self.encoder_linear(e3_enc.permute(0,2,1))
-        # print("Posterior: ", posterior)
         posterior = self.sigmoid_activation(posterior/10.)
         sampler = torch.distributions.continuous_bernoulli.ContinuousBernoulli(probs=posterior)
-        # print("3. posterior shape: ", posterior.shape)
+        print("3. posterior shape: ", posterior.shape)
 
         sampled_val = sampler.sample()
-        mask = sampled_val.repeat(1, 1, 257)
-        # print("4. mask shape: ", mask.shape)
+        mask = sampled_val.repeat(1, 1, 256)
+        print("4. mask shape: ", mask.shape)
 
-        enc_out = x * mask.permute(0,2,1)
+        enc_out = e3_enc * mask.permute(0,2,1)
         e1_dec = self.elu(self.bn1_dec(self.conv1_dec(enc_out)))
         e2_dec = self.elu(self.bn2_dec(self.conv2_dec(e1_dec)))
         e2_dec = e2_dec.permute(2,0,1)
-        # print("5. e2_dec shape: ", e2_dec.shape)
+        print("5. e2_dec shape: ", e2_dec.shape)
+
         e3_dec, _ = self.decoder_mha(e2_dec, e2_dec, e2_dec)
         e3_dec = e3_dec.permute(1,2,0)
-        # print("6. e3_dec shape: ", e3_dec.shape)
+        print("6. e3_dec shape: ", e3_dec.shape)
+
         e3_dec = self.bn3_dec(e3_dec)
         out = self.decoder_linear(e3_dec.permute(0,2,1))
-        # print("7. out shape: ", out.shape)
+        print("7. out shape: ", out.shape)
 
         return posterior, sampled_val, out.permute(0,2,1)
 
