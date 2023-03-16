79bbcf0d126d4a4d11fb6a748b270348044d8e9f
ravi-0841 <ravishankar0841@gmail.com>
2023-03-16 14:32:54 -0400

    lstm layer in mask generator to smear out features

--- masked_saliency_predictor_TD_RL_trainer_postEmoRate.py
+++ masked_saliency_predictor_TD_RL_trainer_postEmoRate.py
@@ -13,7 +13,7 @@ import torch
 import numpy as np
 from torch.utils.data import DataLoader
 # from saliency_predictor_TD_RL_postEmoRate import MaskedRateModifier, RatePredictor
-from conv_mask_trans_masked_postEmoRate_2 import MaskedRateModifier, RatePredictor
+from lstm_gen_conv_mask_trans_masked_postEmoRate import MaskedRateModifier, RatePredictor
 from on_the_fly_augmentor_raw import OnTheFlyAugmentor, acoustics_collate_raw
 from src.common.loss_function import (MaskedSpectrogramL1LossReduced,
                                         ExpectedKLDivergence,
