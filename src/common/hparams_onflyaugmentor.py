54c523995b0382bf97075ed9f44cb86907e9f06b
ravi-0841 <ravishankar0841@gmail.com>
2023-03-16 11:48:17 -0400

    mixed minibatch with aggressive modification

--- src/common/hparams_onflyaugmentor.py
+++ src/common/hparams_onflyaugmentor.py
@@ -66,7 +66,7 @@ def create_hparams(**kwargs):
         "output_directory": "./masked_predictor_output",  # Directory to save checkpoints.
         # Directory to save tensorboard logs. Just keep it like this.
         "log_directory": './logs',
-        "extended_desc": "minibatch_consistent_conv_mask_6", # extra description for logging/identification
+        "extended_desc": "conv_mask_6", # extra description for logging/identification
         "checkpoint_path": "",  # Path to a checkpoint file.
         "checkpoint_path_inference": "./masked_predictor_output/lr_opposing_1e-05_10.0_2e-07_5.0_AllEmo_minibatch_consistent_conv_mask/checkpoint_145000",
         # "checkpoint_path_inference": "./masked_predictor_output/lr_opposing_1e-05_10.0_2e-07_5.0_AllEmo/checkpoint_145000",  # Path to a checkpoint file.
