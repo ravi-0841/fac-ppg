6b584d2738321b4d22541d08ac057f0e32319f23
ravi-0841 <ravishankar0841@gmail.com>
2023-04-20 16:46:14 -0400

    cremad evaluation using VESUS model done: extremely poor

---
+++ on_the_fly_augmentor_raw_voice_mask_cremad.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 17:30:21 2023
+
+@author: ravi
+"""
+
+
+import os
+import numpy as np
+import soundfile as sf
+import torch
+import pylab
+import pandas as pd
+import ast
+import librosa
+from collections import Counter
+import numpy as np
+import scipy.signal as scisig
+
+from src.common.hparams_onflyenergy_cremad import create_hparams
+from src.common.wsolatsm import wsola
+from src.common.utils import load_filepaths
+from torch.utils.data import DataLoader
+
+class OnTheFlyAugmentor():
+
+    def __init__(
+                    self, 
+                    utterance_paths_file,
+                    hparams,
+                    augment=True,
+                    base_folder=""
+                ):
+        
+        self.utterance_rating_paths = load_filepaths(utterance_paths_file)
+        self.base_folder = base_folder
+        self.augment = augment
+        self.hparams = hparams
+        self.wsola_func = wsola
+
+        
+    def _extract_stft_feats(self, data):
+        data = torch.from_numpy(data).float()
+        stft_features = torch.stft(
+                                    data,
+                                    n_fft=self.hparams.n_fft,
+                                    win_length=self.hparams.win_length,
+                                    hop_length=self.hparams.hop_length,
+                                    return_complex=False,
+                                    )
+        
+        return stft_features
+
+    
+    def _get_random_sr(self, 
+                       lower_sr_factor=0.8, #0.8
+                       upper_sr_factor=1.25): #1.25
+        act_sr = self.hparams.sampling_rate
+        return int(act_sr * (lower_sr_factor + (upper_sr_factor - lower_sr_factor)*np.random.rand()))
+
+
+    def _get_random_factor(self, 
+                           lower_factor=0.75,
+                           upper_factor=1.35):
+        return lower_factor + (upper_factor - lower_factor)*np.random.rand()
+
+
+    def _get_signal(self, path):
+        clean_data, sr = sf.read(os.path.join(self.base_folder, path))
+
+        # if sr != self.hparams.sampling_rate:
+        #     clean_data = librosa.resample(clean_data, sr, self.hparams.sampling_rate)
+        
+        if self.augment:
+            random_sr = self._get_random_sr()
+        else:
+            random_sr = self.hparams.sampling_rate
+
+        clean_data = librosa.resample(clean_data, orig_sr=sr, target_sr=random_sr)
+        energy = librosa.feature.rms(y=clean_data, 
+                                    frame_length=self.hparams.win_length,
+                                    hop_length=self.hparams.hop_length,
+                                    center=True)
+        energy = energy.reshape(-1,)
+        voice_mask = np.zeros((len(energy,)))
+        voice_mask[np.where(energy>1e-3)[0]] = 1
+        idx = np.where(voice_mask==1)[0]
+        voice_mask[idx[0]:idx[-1]] = 1
+        voice_mask = np.multiply(voice_mask, energy)
+        # voice_mask[idx[0]:] = 1
+        # voice_mask[:idx[-1]] = 1
+
+        return clean_data.reshape(1,-1), voice_mask.reshape(1,-1), random_sr
+
+
+    def _get_signal_factor(self, path):
+        clean_data, sr = sf.read(os.path.join(self.base_folder, path))
+
+        # if sr != self.hparams.sampling_rate:
+        #     clean_data = librosa.resample(clean_data, sr, self.hparams.sampling_rate)
+        
+        clean_data = librosa.resample(clean_data.reshape(-1,), 
+                                      orig_sr=sr, 
+                                      target_sr=self.hparams.sampling_rate)
+        
+        if self.augment:
+            random_factor = self._get_random_factor()
+            clean_data = self.wsola_func(x=clean_data, s=random_factor, 
+                                         win_size=self.hparams.win_length,
+                                         syn_hop_size=self.hparams.hop_length,
+                                         tolerance=self.hparams.hop_length,
+                                         )
+        else:
+            random_factor = 1
+        
+        energy = librosa.feature.rms(y=clean_data, 
+                                    frame_length=self.hparams.win_length,
+                                    hop_length=self.hparams.hop_length,
+                                    center=True)
+        energy = energy.reshape(-1,)
+        voice_mask = np.zeros((len(energy,)))
+        voice_mask[np.where(energy>1e-3)[0]] = 1
+        idx = np.where(voice_mask==1)[0]
+        voice_mask[idx[0]:idx[-1]] = 1
+        voice_mask = np.multiply(voice_mask, energy)
+        # voice_mask[idx[0]:] = 1
+        # voice_mask[:idx[-1]] = 1
+
+        return clean_data.reshape(1,-1), voice_mask.reshape(1,-1), random_factor
+    
+    
+    def _rating_structure(self, rating):
+        rate_dict = {0:0, 1:0, 2:0, 3:0, 4:0}
+        for r in rating:
+            rate_dict[r] += 1
+        return np.asarray(list(rate_dict.values())).reshape(1,-1) / 10.
+
+
+    def __getitem__(self, index):
+        path = self.utterance_rating_paths[index]
+        speech_data, voice_mask, sr = self._get_signal_factor(path) # self._get_signal()
+        speech_stft = self._extract_stft_feats(speech_data)
+        speech_stft = torch.sqrt(speech_stft[:,:,0]**2 + speech_stft[:,:,1]**2)
+        return (
+                speech_stft,
+                torch.from_numpy(speech_data).float(),
+                torch.from_numpy(voice_mask).float(),
+                sr,
+                path,
+        )
+
+
+    def __len__(self):
+        return len(self.utterance_rating_paths)
+
+
+def acoustics_collate_raw(batch):
+    """Zero-pad the acoustic sequences in a mini-batch.
+
+    Args:
+        batch: An array with B elements, each is a tuple 
+        (stft, speech_wav, tar_rating, sampling_rate).
+
+    Returns:
+        speech_padded: A (batch_size, 1, num_samples)
+        tensor.
+        voice_masks: (batch_size, 1, num_frames)
+        lengths: A batch_size array, each containing the actual length
+        of the input sequence.
+    """
+    # Right zero-pad all PPG sequences to max input length.
+    # x is (PPG, acoustic), x[0] is PPG, which is an (L(varied), D) tensor.
+    input_lengths, ids_sorted_decreasing = torch.sort(
+        torch.LongTensor([x[1].shape[1] for x in batch]), dim=0,
+        descending=True)
+    max_input_len = input_lengths[0]
+
+    input_lengths_mask, _ = torch.sort(
+        torch.LongTensor([x[2].shape[1] for x in batch]), dim=0,
+        descending=True)
+    max_input_len_mask = input_lengths_mask[0]
+
+    speech_padded = torch.FloatTensor(len(batch), 1, max_input_len)
+    voice_mask_padded = torch.FloatTensor(len(batch), 1, max_input_len_mask)
+    path_list = []
+    
+    speech_padded.zero_()
+    voice_mask_padded.zero_()
+    
+    for i in range(len(ids_sorted_decreasing)):
+        curr_speech = batch[ids_sorted_decreasing[i]][1]
+        curr_voice_mask = batch[ids_sorted_decreasing[i]][2]
+        curr_path = batch[ids_sorted_decreasing[i]][4]
+
+        speech_padded[i, :, :curr_speech.shape[1]] = curr_speech
+        speech_padded[i, :, curr_speech.shape[1]:] = 0
+        
+        voice_mask_padded[i, :, :curr_voice_mask.shape[1]] = curr_voice_mask
+        voice_mask_padded[i, :, curr_voice_mask.shape[1]:] = 0
+
+        path_list.append(curr_path)
+
+    return speech_padded, voice_mask_padded, path_list, input_lengths
+
+
+if __name__ == "__main__":
+    hparams = create_hparams()
+
+    dataclass = OnTheFlyAugmentor(
+                                utterance_paths_file="./speechbrain_data/cremad_train.txt",
+                                hparams=hparams,
+                                augment=False,
+                                )
+
+    print(dataclass[10][1].shape)
+    dataloader = DataLoader(
+                            dataclass,
+                            num_workers=1,
+                            shuffle=True,
+                            sampler=None,
+                            batch_size=8,
+                            drop_last=True,
+                            collate_fn=acoustics_collate_raw,
+                            )
+    for i, batch in enumerate(dataloader):
+        signal = batch[0][2].cpu().numpy().reshape(-1,)
+        voice_mask = batch[1][2].cpu().numpy().reshape(-1,)
+        name = batch[2][2]   
+        
+        pylab.xticks(fontsize=18)
+        pylab.yticks(fontsize=18)
+        fig, ax = pylab.subplots(2, 1, figsize=(30, 10))
+        
+        ax[0].plot(signal, linewidth=1.5, color='k')
+        ax[0].set_xlabel('Time',fontsize=15) #xlabel
+        ax[0].set_ylabel('Magnitude', fontsize=15) #ylabel
+
+        ax[1].plot(voice_mask, linewidth=2.5)
+        ax[1].set_xlabel('Time',fontsize=15) #xlabel
+        ax[1].set_ylabel('Threshold', fontsize=15) #ylabel
+        
+        pylab.suptitle("Utterance {}, name-{}".format(i+1, name))
+        
+        pylab.savefig("./test_images_2/{}.png".format(i+1))
+        pylab.close()
+        
+        # print(torch.div(batch[2], 160, rounding_mode="floor") - len(energy))
+        # print(batch[2].dtype, torch.div(batch[2], 160, rounding_mode="floor").dtype)
+        
+        if i >= 100:
+            break
+
+    # src_stft, l, src = dataloader[19]
+
+    # print("STFT shape: ", src_stft.shape)
+    # print("length_array: ", l)
+    # print("audio shape: ", src.shape)
+    
+    # pylab.figure()
+    # pylab.imshow(np.log10(src_stft[:,:,0]**2 + src_stft[:,:,1]**2), origin="lower")
+    # pylab.figure()
+    # pylab.plot(src)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/common/hparams_onflyenergy_cremad.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 16:14:38 2023
+
+@author: ravi
+"""
+
+
+# BSD 3-Clause License
+#
+# Copyright (c) 2018, NVIDIA Corporation
+# Copyright (c) 2019, Guanlong Zhao
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Modified from https://github.com/NVIDIA/tacotron2"""
+
+
+class HParamsView(object):
+    def __init__(self, d):
+        self.__dict__ = d
+
+
+def create_hparams(**kwargs):
+    """Create model hyperparameters. Parse nondefault from given string."""
+
+    hparams = {
+        ################################
+        # Experiment Parameters        #
+        ################################
+        "epochs": 200,
+        "iters_per_checkpoint": 1000,
+        "seed": 1107,
+        "dynamic_loss_scaling": True,
+        "fp16_run": False,
+        "distributed_run": False,
+        "dist_backend": "nccl",
+        "dist_url": "tcp://localhost:54321",
+        "cudnn_enabled": True,
+        "cudnn_benchmark": False,
+        "output_directory": "./masked_predictor_output",  # Directory to save checkpoints.
+        # Directory to save tensorboard logs. Just keep it like this.
+        "log_directory": './logs',
+        "extended_desc": "BiLSTM_maskGen_evm_wsola_aug", # extra description for logging/identification
+        "checkpoint_path": "", #"./masked_predictor_output/noPost_1e-05_10.0_2e-07_5.0_mask_trans_rate_beta_0.75_mix_entropy_eval/checkpoint_193500",  # Path to a checkpoint file.
+        "checkpoint_path_saliency": "./masked_predictor_output/1e-05_10.0_0.0002_5.0_BiLSTM_maskGen/checkpoint_102000",
+        "checkpoint_path_rate": "",
+        "checkpoint_path_inference": "./masked_predictor_output/1e-05_10.0_0.0002_5.0_BiLSTM_maskGen_evm_wsola_aug/checkpoint_78000", #"./masked_predictor_output/1e-05_10.0_0.0002_5.0_BiLSTM_maskGen_energy_value_mask/checkpoint_154000",
+        "warm_start": False,  # Load the model only (warm start)
+        "n_gpus": 1,  # Number of GPUs
+        "rank": 0,  # Rank of current gpu
+        "group_name": 'group_name',  # Distributed group name
+
+        ################################
+        # Data Parameters             #
+        ################################
+        # Passed as a txt file, see data/filelists/training-set.txt for an
+        # example.
+        "complete_files": "./speechbrain_data/VESUS_saliency_complete.txt",
+        "training_files": './speechbrain_data/cremad_train.txt',
+        "validation_files": './speechbrain_data/cremad_valid.txt',
+        "testing_files": "./speechbrain_data/cremad_test.txt",
+        "is_full_ppg": True,  # Whether to use the full PPG or not.
+        "is_append_f0": False,  # Currently only effective at sentence level
+        "ppg_subsampling_factor": 1,  # Sub-sample the ppg & acoustic sequence.
+        # Cases
+        # |'load_feats_from_disk'|'is_cache_feats'|Note
+        # |True                  |True            |Error
+        # |True                  |False           |Please set cache path
+        # |False                 |True            |Overwrite the cache path
+        # |False                 |False           |Ignores the cache path
+        "load_feats_from_disk": False,  # Remember to set the path.
+        # Mutually exclusive with 'load_feats_from_disk', will overwrite
+        # 'feats_cache_path' if set.
+        "is_cache_feats": False,
+        "feats_cache_path": '',
+
+        ################################
+        # Audio Parameters             #
+        ################################
+        "max_wav_value": 32768.0,
+        "sampling_rate": 16000,
+        "downsampling_factor": 160,
+        "n_acoustic_feat_dims": 40, #80
+        "filter_length": 400, #1024
+        "n_fft": 512,
+        "hop_length": 160,
+        "win_length": 320, #1024
+        "mel_fmin": 0.0,
+        "mel_fmax": 8000.0,
+
+        ################################
+        # Optimization Hyperparameters #
+        ################################
+        "lambda_prior_KL": 1e-5, # 5e-4
+        "lambda_predict": 10.0, # 10
+        "lambda_sparse_KL": 2e-04, # 1e-07
+        "lambda_entropy": 1,
+        "temp_scale": 5.0, #15.0
+        "use_saved_learning_rate": False,
+        "learning_rate_saliency": 1e-4, #1e-5
+        "learning_rate_rate": 1e-7, #1e-6
+        "learning_rate_decay": 0.97724, #0.955
+        "learning_rate_lb": 1e-6, #1e-6
+        "learning_rate_ub": 1e-5, #1e-4
+        "weight_decay": 1e-7, #1e-6
+        "grad_clip_thresh": 1.0,
+        "batch_size": 8, #8
+        "minibatch_consistency": False,
+        "mask_padding": True, # set model's padded outputs to padded values
+        "alpha": 0.01, # Bernoulli parameter for sampling 1st entry of the mask
+        "beta": 0.95, #0.95 Bernoulli parameter for mask persistence
+    }
+
+    for key, val in kwargs.items():
+        if key in hparams:
+            hparams[key] = val
+        else:
+            raise ValueError('The hyper-parameter %s is not supported.' % key)
+
+    hparams_view = HParamsView(hparams)
+
+    return hparams_view
+
+
